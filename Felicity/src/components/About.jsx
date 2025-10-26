4d7d5709d5418d8451e9af9ed3d6ed318d550ce2
mellow874 <felicitymncube49@gmail.com>
2025-10-26 15:20:58 +0200

    Created the About section

---
+++ Felicity/src/components/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About () {
+  return (
+    <section id="about" className="about-section">
+      <div className="about-container">
+        <h2 className="about-title">About Me 💫</h2>
+        <p className="about-text">
+          My name is <span className="highlight">Felicity Mncube</span>, and I’m currently
+          a student at Melsoft Academy pursuing a qualification in Software Engineering. I have a
+          deep passion for technology and innovation,and through code I'm able to create meaningful 
+          experiences, solving real world problems, and making a positive impact to the world  
+        </p>
+
+        <p className="about-text">
+          I completed my matric in 2023, and the following year, I decided to
+          explore the world of programming. I started by learning the basics of of web development,
+          and just building simple projects, and since then I've been growing from there. 
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default About;
