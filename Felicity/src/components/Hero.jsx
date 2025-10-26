2af92499e7daa36b690d83fbbaaf6c3a89fcaf1a
mellow874 <felicitymncube49@gmail.com>
2025-10-26 13:17:45 +0200

    Installed Typewriter,, and imported it along with the social icons

--- Felicity/src/components/Hero.jsx
+++ Felicity/src/components/Hero.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
-const Hero = () => {
+
+export default function Hero() {
   return (
-    <section id="home" className="home-section">
-      <div className="intro">
-        <h1 className="greeting">Hi, I'm <span className="highlight">Felicity Mncube</span></h1>
-        <h2 className="intro-text">
-          a Software Development student crafting seamless digital experiences with creativity, precision, and purpose.
-        </h2>
-
-        <div className="buttons">
-          <a href="/mellowCV.pdf" download className="btn neon">Download CV</a>
-          <a href="#contact" className="btn outline">Contact Me</a>
-        </div>
-        <div className="socials">
-            <a href="https://github.com/mellow874" target="_blank" rel="noreferrer">GitHub</a>
-            <a href="https://www.instagram.com/mellow.874/" target="_blank" rel="noreferrer">Instagram</a>
-            <a href="https://www.linkedin.com/in/felicity-mncube-65b796322/" target="_blank" rel="noreferrer">LinkedIn</a>  
-        </div>
-      </div>
-    </section>
+    
   );
-};
-
-export default Hero;
+}
